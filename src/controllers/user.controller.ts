b093e2beec4eab1414c2639ea79416cf75682ee8
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-04-28 09:50:47 +0300

    Docs User Controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,34 @@
+import { UserService } from "../services/user.service";
+import { Request, Response } from "express";
+
+
+export class UserController {
+    private userService: UserService;
+    
+    constructor() {
+        this.userService = new UserService();
+    }
+
+    async getUsers(req: Request, res: Response) {
+        const users = await this.userService.getUsers();
+        res.status(200).json(users);
+    }
+
+    async createUser(req: Request, res: Response) {
+        const user = await this.userService.createUser(req.body);
+        res.status(201).json(user);
+    }
+
+    async updateUser(req: Request, res: Response) {
+        const userId  = +(req.params.id);
+        const user = await this.userService.updateUser(userId, req.body);
+        res.status(200).json(user);
+    }
+
+    async deleteUser(req: Request, res: Response) {
+        const userId  = +(req.params.id);
+        await this.userService.deleteUser(userId);
+        res.status(204).send();
+    }
+
+}
